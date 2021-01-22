1ce23376387bcaecb968ee093d89bbe9144abab6
Deeya S Doshi <65722056+deeyasdoshi@users.noreply.github.com>
2021-01-22 18:17:02 +0530

    Add files via upload

---
+++ backend/data.js
@@ -0,0 +1,46 @@
+export default {
+  products: [
+    {
+      _id: '1',
+      name: 'Pink Cotton Shirt',
+      category: 'Shirts',
+      image: '/images/pink cotton.jpg',
+      price: 60,
+      brand: ' Nike',
+      rating: 4.5,
+      numReviews: 10,
+      countInStock: 6,
+    },
+    {
+      _id: '2',
+      name: 'Classy Shirt',
+      category: 'Shirts',
+      image: '/images/classy shirt.jpg',
+      price: 50,
+      brand: ' Lyra',
+      rating: 4.2,
+      numReviews: 5
+    },
+    {
+      _id: '3',
+      name: 'Black Scree Pants',
+      category: 'Pants',
+      image: '/images/black scree.jpg',
+      price: 70,
+      brand: ' Ginger',
+      rating: 4.5,
+      numReviews: 8,
+      countInStock: 6,
+    }, {
+      _id: '4',
+      name: 'Leather Pants',
+      category: 'Pants',
+      image: '/images/leather pants.jpg',
+      price: 70,
+      brand: ' Nike',
+      rating: 4.5,
+      numReviews: 8,
+      countInStock: 6,
+    },
+  ]
+}
